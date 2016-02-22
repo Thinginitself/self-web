9e962595c0048464f51256bcc395bef6ad22c5ea
Thinginitself <thinginitself@itself.local>
2016-02-23 00:01:36 +0800

    nothing changed

--- prepare.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sh ./skyspace/run-tuplespace.sh &
-python ./ResPool/res_pool.py &
-python ./test_agent.py &
--- skyspace/run-tuplespace.sh
+++ skyspace/run-tuplespace.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Djava.net.preferIPv4Stack=true -jar ./java-sky-1.0-SNAPSHOT.jar com.skyspace.restfulsky.Application
+java -Djava.net.preferIPv4Stack=true -jar ./skyspace/java-sky-1.0-SNAPSHOT.jar com.skyspace.restfulsky.Application
