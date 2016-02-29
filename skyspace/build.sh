8e33d26d8c5f5c9a068eec50ef1932ccfae449bb
Thinginitself <thinginitself@itself.local>
2016-03-01 02:04:19 +0800

    remove skyspace

--- skyspace/build.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-
-cd ..
-mvn clean package
-cp target/java-sky-1.0-SNAPSHOT.jar bin/
--- skyspace/run-tuplespace.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -Djava.net.preferIPv4Stack=true -jar ./skyspace/java-sky-1.0-SNAPSHOT.jar com.skyspace.restfulsky.Application
